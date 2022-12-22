80ca8fcd13e4860bc09baa8fbd2af83fab89e652
Sven <me@neddo.dev>
2022-12-22 14:26:26 +0100

    added reload button and logo on PC browsers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
+  import MdReplay from 'svelte-icons/md/MdReplay.svelte'
 	const fetchInfo = (async () => {
 		const response = await fetch('https://mc-api.grphcrtv.com/v1/ping/play.originrealms.com');
 		return await response.json();
 	})();
 
-  // @ts-ignore
+	// @ts-ignore
 
 	const copyContent = async () => {
 		try {
-      // @ts-ignore
+			// @ts-ignore
 			await navigator.clipboard.writeText(document.getElementById('serverIP').innerHTML);
 			console.log('Content copied to clipboard');
 			// @ts-ignore
@@ -24,18 +25,33 @@
 </script>
 
 <main class="p-5">
-	<div class="flex-col space-y-2 mb-5">
-		<h1 class="font-black text-3xl">Origin Realms Status Tracker</h1>
-		<p class="text-lg opacity-50">
-			Made by <a class="underline hover:opacity-75 duration-200" href="https://github.com/Neddoo"
-				>Neddo</a
-			>
-		</p>
+	<div class="flex flex-row justify-between">
+		<div class="flex flex-row">
+			<div class="hidden lg:block">
+				<img
+					class="w-20 h-20"
+					src="https://cdn.discordapp.com/icons/803581115024670740/db84373774700b380b4fee2a5209191e.webp?size=96"
+					alt="Logo"
+				/>
+			</div>
+			<div class="flex-col space-y-2 mb-5 my-auto lg:ml-2">
+				<h1 class="font-black text-3xl">Origin Realms Status Tracker</h1>
+				<p class="text-lg opacity-50">
+					Made by <a
+						class="underline hover:opacity-75 duration-200"
+						href="https://github.com/Neddoo">Neddo</a
+					>
+				</p>
+			</div>
+		</div>
+    <div>
+      <button class="w-10 h-10 mt-3" on:click={() => {location.reload();}}><MdReplay /></button>
+    </div>
 	</div>
 	{#await fetchInfo}
 		<p>Fetching data...</p>
 	{:then data}
-		<div class="grid grid-cols-3 space-x-2">
+		<div class="flex-col flex lg:grid grid-cols-3 lg:space-x-2 space-y-2 lg:space-y-0">
 			<div class="border-neutral-700 border-2 rounded-lg">
 				<div class="p-5 flex flex-col space-y-2">
 					<h3 class="text-xl font-bold">Online Players</h3>
@@ -52,7 +68,7 @@
 				<div class="p-5 flex flex-col space-y-2">
 					<h3 class="text-xl font-bold">IP</h3>
 					<div class="text-xl opacity-75">
-						<div class="flex space-x-1 justify-between">
+						<div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 justify-between">
 							<p id="serverIP">{data.hostname}</p>
 							<button
 								class="border-neutral-600 border-2 rounded-lg px-3"
